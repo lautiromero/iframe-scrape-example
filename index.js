fb2981a12054d0ee24933cbc1263f6be37fb3729
lautiromero <lautiromero94@gmail.com>
2022-12-02 17:01:17 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import puppeteer from 'puppeteer';
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless:false,
+    args: [
+      '--disable-web-security',
+      '--disable-features=IsolateOrigins,site-per-process'
+    ]
+  });
+  const page = await browser.newPage();
+
+  await page.goto('https://www.w3schools.com/html/html_iframe.asp', {
+    waitUntil: "networkidle0",
+    timeout: 30000
+  });
+
+
+  const frameHandle = await page.$("iframe[title='W3Schools HTML Tutorial']");
+
+  const frame = await frameHandle.contentFrame();
+
+
+  // const frames = await page.frames();
+  // const frame = frames.find(f => f.url().includes('default'));
+
+  const h1Contents = await frame.$$eval('h1', elements => elements.map(el => el.textContent))
+
+  console.log(h1Contents[0])
+
+})()
